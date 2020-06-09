589d2531141bc5fd7cc17dd8d3c70247f0c6e14b
Apirak Suwanyotee <dark.holy40@gmail.com>
2020-06-09 17:23:40 +0700

    - [Add] added Home page // display only awards

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,157 @@
 import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
+import axios from 'axios'
 import styled from 'styled-components'
 import {
     Col
 } from 'antd'
 import 'antd/dist/antd.min.css'
+import useInterval from '../components/functions/useInterval'
 
 import MainContainer from '../components/layouts/MainContainer'
 import MainRow from '../components/layouts/MainRow'
+import CardShield from '../components/layouts/CardShield'
+import Card from '../components/layouts/Card'
+
+const StyledCardShield = styled(CardShield)`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+const StyledCard = styled(Card)`
+    width: 960px;
+`
+
+const NextAward = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+
+    p.block {
+        margin: 0;
+    }
+
+    span.next-award {
+        font-size: 1.5rem;
+        padding: 0 0.5rem;
+        animation-duration: 2s;
+        animation-name: ${props => props.theme === 'sun' ? 'highlight-yellow-day' : 'highlight-yellow-night'};
+        animation-delay: 0;
+        animation-iteration-count: infinite;
+        animation-direction: forward;
+    }
+
+    span.remain-awards {
+        font-size: 1.5rem;
+        padding: 0 0.5rem;
+        animation-duration: 2s;
+        animation-name: ${props => props.theme === 'sun' ? 'highlight-blue-day' : 'highlight-blue-night'};
+        animation-delay: 0;
+        animation-iteration-count: infinite;
+        animation-direction: forward;
+    }
+
+    .dummy {
+        opacity: 0;
+    }
+
+    @media (max-width: 767px) {
+        flex-direction: column;
+
+        .dummy {
+            display: none;
+        }
+    }
+`
+
+const Label = styled.p`
+    text-align: left;
+    font-weight: 500;
+    border-bottom: 1px solid ${props => props.theme === 'sun' ? 'rgba(0, 0, 0, 0.1)' : 'rgba(255, 255, 255, 0.2)'};
+`
+
+const PersonsAmountBlock = styled.div`
+    margin-top: 1.5rem;
+    margin-bottom: 1.5rem;
+
+    div {
+        height: 300px;
+        overflow: auto;
+        text-align: left;
+        margin: 0 5rem;
+
+        p {
+            margin: 0;
+
+            &.is-picked-up {
+                background-color: rgba(0, 255, 0, 0.5);
+            }
+        }
+        
+        @media (max-width: 991px) {
+            margin: 0;
+
+            p {
+                white-space: nowrap;
+                overflow: hidden;
+                text-overflow: ellipsis;
+            }
+        }
+    }
+`
+
+const ListItemsNotificationText = styled.p`
+    text-align: center;
+    font-size: 0.85rem;
+    color: ${props => props.theme === 'sun' ? 'rgba(0, 0, 0, 0.3)' : 'rgb(175, 175, 175)'};
+    margin: 0;
+    display: ${props => props.display || 'none'};
+    transition: 0.3s;
+`
+
+const AwardsResult = styled.div`
+    text-align: left;
+
+    p {
+        display: flex;
+        
+        span.left {
+            width: 40%;
+            white-space: nowrap;
+            overflow: hidden;
+            text-overflow: ellipsis;
+        }
+
+        span.right {
+            width: 60%;
+
+            span.first-picked-up {
+                animation-duration: 2s;
+                animation-name: ${props => props.theme === 'sun' ? 'highlight-green-day' : 'highlight-green-night'};
+                animation-delay: 0;
+                animation-iteration-count: infinite;
+                animation-direction: forward;
+            }
+        }
+    }
+`
 
 function mapStateToProps(state) {
     return state
 }
 
 function Home(props) {
+    const [firstCardClass, setFirstCardClass] = useState('hidden')
+    const [secondCardClass, setSecondCardClass] = useState('hidden')
+
+    const [awardsList, setAwardsList] = useState({})
+
+    const classNames = {
+        first: window.innerWidth < 768 ? "animated fadeInUp" : "animated fadeInDown",
+        second: window.innerWidth < 768 ? "animated fadeInUp" : "animated fadeIn",
+    }
+
     useEffect(() => {
         props.dispatch({
             type: 'SET_BREADCRUMB',
@@ -24,12 +162,86 @@ function Home(props) {
                 }
             ]
         })
+
+        fetchData()
+
+        setTimeout(() => {
+            setFirstCardClass(classNames.first)
+            setSecondCardClass(classNames.second)
+        }, 150)
     }, [])
+
+    useInterval(() => {
+        fetchData()
+    }, 1000)
+
+    function fetchData() {
+        // ดึกข้อมูล รายการรางวัลทั้งหมด
+        axios.get(`${props.url}/getawardslist`)
+        .then(res => {
+            const response = res.data
+            
+            if(response.code === "00200") {
+                // console.log(response.data)
+                setAwardsList(response.data)
+            }
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }
 
     return (
         <MainContainer className="animated fadeIn">
             <MainRow>
-                
+                <Col xs={24}>
+                    <NextAward theme={props.theme}>
+                        <CardShield className="dummy">
+                            <Card>
+                                จำนวนรางวัลคงเหลือ: {awardsList.remain}
+                            </Card>
+                        </CardShield>
+                        <CardShield className={firstCardClass}>
+                            <Card>
+                                <p className="block">
+                                    {awardsList.remain > 0 ?
+                                    <>รางวัลรายการถัดไป: <span className="next-award">{awardsList.data.awards_remain[0].name}</span></> :
+                                    <span className="next-award">จับรางวัลหมดแล้ว</span>
+                                    }
+                                </p>
+                            </Card>
+                        </CardShield>
+                        <CardShield className={firstCardClass}>
+                            <Card>
+                                จำนวนรางวัลคงเหลือ: <span className="remain-awards">{awardsList.remain}</span>
+                            </Card>
+                        </CardShield>
+                    </NextAward>
+                </Col>
+            </MainRow>
+            <MainRow>
+                <Col md={24} sm={24}>
+                    <StyledCardShield className={secondCardClass}>
+                        <StyledCard theme={props.theme}>
+                            <Label theme={props.theme}>ผลการจับรางวัล</Label>
+                            <ListItemsNotificationText display={awardsList.remain === awardsList.max ? 'block' : 'none'} theme={props.theme}>
+                                ยังไม่ทำการจับรางวัล
+                            </ListItemsNotificationText>
+                            <AwardsResult theme={props.theme}>
+                                {Object.keys(awardsList).length > 0 && awardsList.data.persons_whom_are_picked_up.map((pickedupPerson, pickedupPersonIndex) => {
+                                    return (
+                                        <p key={pickedupPersonIndex}>
+                                            <span className="left">{pickedupPerson.name}</span>
+                                            <span className="right">
+                                                <span className={pickedupPersonIndex === 0 ? "first-picked-up" : ""}>{pickedupPerson.fullname}</span>
+                                            </span>
+                                        </p>
+                                    )
+                                })}
+                            </AwardsResult>
+                        </StyledCard>
+                    </StyledCardShield>
+                </Col>
             </MainRow>
         </MainContainer>
     )
