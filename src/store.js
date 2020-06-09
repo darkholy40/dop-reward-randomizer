dd112b19a6330eb3fcad2fe41781c4bc16bfaac7
Apirak Suwanyotee <dark.holy40@gmail.com>
2020-06-09 15:09:54 +0700

    - [Add] added install axios

--- src/store.js
+++ src/store.js
@@ -22,7 +22,8 @@ const initState = {
     breadcrumb: [],
     utcMultipliers: multipliers,
     newDate: getUniversalCoordinatedTime(),
-    theme: 'sun'
+    theme: 'sun',
+    url: 'http://localhost:5010'
 }
 
 const reducer = (state = initState, action) => {
